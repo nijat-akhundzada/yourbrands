1fe36ef01ab98f2994bcf9844ba2dcd9e9bbaa79
Nicat-Axundzada <nicataxundzada@gmail.com>
2024-04-19 12:35:13 +0400

    Adding images and updating product models

--- app/api/models.py
+++ app/api/models.py
@@ -14,6 +14,47 @@ class BaseModel(models.Model):
 class Brand(BaseModel):
     name = models.CharField(max_length=255)
 
+    def __str__(self):
+        return self.name
+
+
+class ParentCategory(BaseModel):
+    name = models.CharField(max_length=255)
+
+    def __str__(self):
+        return self.name
+
+
+class Category(BaseModel):
+    name = models.CharField(max_length=255)
+    parent_category = models.ForeignKey(
+        ParentCategory, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return f'Parent category: {self.parent_category}, category: {self.name}'
+
+
+class Subcategory(BaseModel):
+    name = models.CharField(models.Model)
+    category = models.ForeignKey(Category, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return f'Category: {self.category}, subcategory: {self.name}'
+
+
+class Size(models.Model):
+    size = models.CharField(max_length=255)
+
+    def __str__(self):
+        return self.size
+
+
+class Color(models.Model):
+    color = models.CharField(max_length=255)
+
+    def __str__(self):
+        return self.color
+
 
 class Product(BaseModel):
     GENDER_CHOICES = (
@@ -22,25 +63,50 @@ class Product(BaseModel):
         (3, 'boy'),
         (4, 'girl')
     )
-    CATEGORIES_CHOICES = (
-        (1, "Clothing"),
-        (2, "Shoe"),
-        (3, "Beauty"),
-        (4, "Accessory")
-
-    )
     name = models.CharField(max_length=255)
     gender = models.CharField(max_length=255, choices=GENDER_CHOICES)
-    categories = models.CharField(
-        max_length=255, choices=CATEGORIES_CHOICES)  # model
-    subcategories = models.CharField(max_length=255)  # model
-    discount = models.FloatField()
+    brand = models.ForeignKey(
+        Brand, on_delete=models.CASCADE, blank=True, null=True)
+    parent_category = models.ForeignKey(
+        ParentCategory, on_delete=models.CASCADE, blank=True, null=True)
+    categories = models.ForeignKey(Category, on_delete=models.CASCADE, blank=True, null=True
+                                   )
+    subcategories = models.ForeignKey(
+        Subcategory, on_delete=models.CASCADE, blank=True, null=True)
+    discount = models.FloatField()  # ?
     price = models.DecimalField(max_digits=10, decimal_places=2)
-    size = models.CharField(max_length=255, blank=True, null=True)
-    color = models.CharField(max_length=255, blank=True, null=True)
+    size = models.ForeignKey(Size, on_delete=models.CASCADE)
+    color = models.ForeignKey(Color, on_delete=models.CASCADE)
     collection = models.CharField(max_length=255, blank=True, null=True)
+    product_code = models.CharField(max_length=255, blank=True, null=True)  # ?
+
+    def __str__(self):
+        return f'{self.name},{self.brand}'
+
+
+class ProductImages(BaseModel):
+    product = models.ForeignKey(
+        Product, on_delete=models.CASCADE, related_name='images')
+    image = models.ImageField(upload_to='product_images')
+
+    def __str__(self):
+        return self.product
 
 
 class FAQ(BaseModel):
     question = models.CharField(max_length=255)
     answer = models.TextField()
+
+
+class TermAndCondition(BaseModel):
+    terms_and_conditions = models.TextField()
+
+    def __str__(self):
+        return 'Our Terms&Conditions'
+
+
+class PrivacyPolicy(BaseModel):
+    privacy_policy = models.TextField()
+
+    def __str__(self):
+        return 'Our Privacy Policy'
--- app/e_commerce/urls.py
+++ app/e_commerce/urls.py
@@ -19,6 +19,9 @@ from django.urls import path, include
 
 from drf_spectacular.views import SpectacularAPIView, SpectacularSwaggerView
 
+from django.conf import settings
+from django.conf.urls.static import static
+
 from rest_framework_simplejwt.views import (
     # TokenObtainPairView,
     TokenRefreshView,
@@ -37,3 +40,9 @@ urlpatterns = [
     path('user/token/', MyTokenObtainPairView.as_view(), name='token_obtain_pair'),
     path('user/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
 ]
+
+if settings.DEBUG:
+    urlpatterns += static(
+        settings.MEDIA_URL,
+        document_root=settings.MEDIA_ROOT
+    )
